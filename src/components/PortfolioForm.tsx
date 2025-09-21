2e42dd406c4fc05cbbee5dba3935ebd5fc4358fc
Chanakan393 <chana.sch17@gmail.com>
2025-09-21 16:13:45 +0700

    edit reset

--- src/components/PortfolioForm.tsx
+++ src/components/PortfolioForm.tsx
@@ -52,7 +52,7 @@ export default function PortfolioForm() {
                 activityPhotos: undefined,
                 educationLevel: '',
                 school: '',
-                gpa: '', // ให้ว่าง
+                gpa: 0, 
                 university: '',
                 faculty: '',
                 major: '',
